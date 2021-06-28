c54c360b012e2ac696377697a916e82df142c6de
LeandroMatonte <leandromatonte99@gmail.com>
2021-06-28 16:47:42 +0000

    vista asociados

--- src/js/component/lista.js
+++ src/js/component/lista.js
@@ -1,36 +1,59 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
 import PropTypes from "prop-types";
+import { Link, useLocation } from "react-router-dom";
 
 export const Lista = props => {
 	const { store, actions } = useContext(Context);
+	const location = useLocation();
 	return (
 		<div className="lista p-4 shadow my-2">
-			{store[props.tipo].length != 0
-				? store[props.tipo].map(itemLista => {
-						return (
-							<div key={itemLista.id} className="itemLista my-2 p-3 shadow">
-								<div className="row">
-									<div className="col-sm-12 col-md-8">
-										<div className="h-100 d-flex align-items-center">
-											<h3 className="text-light m-0">{itemLista.nombre}</h3>
-										</div>
+			{store[props.tipo].length != 0 ? (
+				store[props.tipo].map(itemLista => {
+					return (
+						<div key={itemLista.id} className="itemLista my-2 p-3 shadow">
+							<div className="row">
+								<div className="col-sm-12 col-md-8">
+									<div className="h-100 d-flex align-items-center">
+										<h3 className="text-light m-0">{itemLista.nombre}</h3>
 									</div>
-									<div className="col-sm-12 col-md-4">
-										<div className="d-flex justify-content-center">
+								</div>
+								<div className="col-sm-12 col-md-4">
+									<div className="d-flex flex-column flex-md-row justify-content-end">
+										{props.tipo == "empresas" ? (
+											<Link to="/empresas/asociados">
+												<button type="button" className="btn botonOutline m-1">
+													Asociados
+												</button>
+											</Link>
+										) : (
+											""
+										)}
+
+										{location.pathname == "/empresas/agregar-asociado" ? (
+											<button type="button" className="btn botonOutline m-1">
+												Agregar
+											</button>
+										) : props.tipo != "asociados" ? (
 											<button type="button" className="btn botonOutline m-1">
 												Editar
 											</button>
+										) : (
 											<button type="button" className="btn botonOutline m-1">
 												Eliminar
 											</button>
-										</div>
+										)}
 									</div>
 								</div>
 							</div>
-						);
-				  })
-				: "hol"}
+						</div>
+					);
+				})
+			) : (
+				<div className="itemLista my-2 p-3 shadow">
+					<h2 className="text-light">Aún no hay datos.</h2>
+				</div>
+			)}
 		</div>
 	);
 };
---
+++ src/js/views/asociadosEmpresa.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { Lista } from "../component/lista";
+import { Link } from "react-router-dom";
+
+export const AsociadosEmpresa = () => (
+	<div className="container">
+		<div className="row mt-3">
+			<div className="col-md-8 col-sm-12">
+				<h1>Asociados</h1>
+			</div>
+			<div className="col-md-4 col-sm-12">
+				<div className="d-flex h-100 justify-content-center justify-content-md-end align-items-center">
+					<Link to="/empresas/agregar-asociado">
+						<button type="button" className="btn boton py-2 my-1">
+							Agregar asociado
+						</button>
+					</Link>
+				</div>
+			</div>
+			<div className="col-12">
+				<div className="py-2">
+					<input
+						type="text"
+						placeholder="Buscar Persona"
+						className="inputGris form-control my-1"
+						id="buscador"
+					/>
+				</div>
+			</div>
+		</div>
+		<Lista tipo="asociados" />
+	</div>
+);
