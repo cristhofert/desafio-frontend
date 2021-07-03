fc023f8bdf6a770b9fb26f80ed59dc332646c261
cristhofert <cristhofert97@gmail.com>
2021-07-03 17:10:37 +0000

    Login y cambio cuando se logea

--- src/js/component/sidebar.js
+++ src/js/component/sidebar.js
@@ -1,45 +1,66 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useContext } from "react";
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
+import { ToLogout } from "./ToLogout";
+import { Context } from "../store/appContext";
 
 export const Sidebar = props => {
+	const { store, actions } = useContext(Context);
+
 	return (
 		<div>
 			<nav className="sidebar h-100">
-				<div className="d-flex justify-content-center align-items-center text-light" style={{ height: "80px" }}>
-					<h1>Logo</h1>
+				<div
+					className="d-flex justify-content-center align-items-center text-light"
+					style={{ height: "110px" }}>
+					<img
+						alt="Centro Comercial e Industrial de San José"
+						src="https://ccisanjose.com.uy/wp-content/uploads/2021/04/cropped-CCISJ-logo-sin-fondo-blanco-e1618519493910.png"
+					/>
 				</div>
-				<Link to="/empresas">
-					<div className="itemListaSidebar d-flex align-items-center justify-content-center">
-						<p>Empresas</p>
-					</div>
-				</Link>
-				<Link to="/personas">
-					<div className="itemListaSidebar d-flex align-items-center justify-content-center">
-						<p>Personas</p>
-					</div>
-				</Link>
-				<Link to="/departamentos">
-					<div className="itemListaSidebar d-flex align-items-center justify-content-center">
-						<div className="text-center">
-							<p>Departamentos y Localidades</p>
-						</div>
-					</div>
-				</Link>
-				<Link to="/asociados">
-					<div className="itemListaSidebar d-flex align-items-center justify-content-center">
-						<div className="text-center">
-							<p>asociados</p>
-						</div>
-					</div>
-				</Link>
-				<Link to="/perfilEmpresa">
-					<div className="itemListaSidebar d-flex align-items-center justify-content-center">
-						<div className="text-center">
-							<p>perfilEmpresa</p>
-						</div>
-					</div>
-				</Link>
+				{store.user.is_admin ? (
+					<>
+						<Link to="/empresas">
+							<div className="itemListaSidebar d-flex align-items-center justify-content-center">
+								<p>Empresas</p>
+							</div>
+						</Link>
+						<Link to="/personas">
+							<div className="itemListaSidebar d-flex align-items-center justify-content-center">
+								<p>Personas</p>
+							</div>
+						</Link>
+						<Link to="/departamentos">
+							<div className="itemListaSidebar d-flex align-items-center justify-content-center">
+								<div className="text-center">
+									<p>Departamentos y Localidades</p>
+								</div>
+							</div>
+						</Link>
+					</>
+				) : (
+					""
+				)}
+				{store.user.empresa ? (
+					<>
+						<Link to="/asociados">
+							<div className="itemListaSidebar d-flex align-items-center justify-content-center">
+								<div className="text-center">
+									<p>Asociados</p>
+								</div>
+							</div>
+						</Link>
+						<Link to="/perfilEmpresa">
+							<div className="itemListaSidebar d-flex align-items-center justify-content-center">
+								<div className="text-center">
+									<p>Perfil Empresa</p>
+								</div>
+							</div>
+						</Link>{" "}
+					</>
+				) : (
+					""
+				)}
 			</nav>
 			<nav className="navbar navbar-light bg-light">
 				<button
@@ -50,6 +71,7 @@ export const Sidebar = props => {
 					className="btn boton m-1">
 					{props.estadoSidebar ? <i className="fas fa-times" /> : <i className="fas fa-bars" />}
 				</button>
+				<ToLogout />
 			</nav>
 		</div>
 	);
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,4 +1,4 @@
-import React, { useRef, useContext } from "react";
+import React, { useRef, useContext, useState } from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 import { Context } from "../store/appContext";
 
@@ -6,51 +6,65 @@ export const Home = () => {
 	const { store, actions } = useContext(Context);
 	const username = useRef(null);
 	const passwd = useRef(null);
+	const [alert, setAlert] = useState("");
 
 	return (
 		<div className="container">
-			<div className="row justify-content-center">
-				<div className="col-6 shadow" id="fondoLogin">
-					<h1 className="mt-3 text-center">Inicio de Sesión</h1>
-					<div className="d-flex justify-content-center">
-						<form className="w-75">
-							<div className="mb-3 mt-3">
-								<label htmlFor="exampleInputEmail1" className="form-label">
-									Nombre de usuario
-								</label>
-								<input
-									type="text"
-									className="form-control"
-									id="exampleInputEmail1"
-									aria-describedby="emailHelp"
-									ref={username}
-								/>
-							</div>
-							<div className="mb-3">
-								<label htmlFor="exampleInputPassword1" className="form-label">
-									Contraseña
-								</label>
-								<input
-									type="password"
-									className="form-control"
-									id="exampleInputPassword1"
-									ref={passwd}
-								/>
-							</div>
-							<div className="justify-content-center d-flex">
-								<button
-									type="button"
-									className="btn btn-light mb-5"
-									onClick={() => {
-										actions.login(username.current.value, passwd.current.value);
-									}}>
-									Aceptar
-								</button>
-							</div>
-						</form>
+			{alert != "" ? (
+				<div className="alert alert-danger" role="alert">
+					{alert}
+				</div>
+			) : (
+				""
+			)}
+			{!sessionStorage.getItem("token") || sessionStorage.getItem("token") === "undefined" ? (
+				<div className="row justify-content-center">
+					<div className="col-6 shadow" id="fondoLogin">
+						<h1 className="mt-3 text-center">Inicio de Sesión</h1>
+						<div className="d-flex justify-content-center">
+							<form className="w-75">
+								<div className="mb-3 mt-3">
+									<label htmlFor="exampleInputEmail1" className="form-label">
+										Nombre de usuario
+									</label>
+									<input
+										type="text"
+										className="form-control"
+										id="exampleInputEmail1"
+										aria-describedby="emailHelp"
+										ref={username}
+									/>
+								</div>
+								<div className="mb-3">
+									<label htmlFor="exampleInputPassword1" className="form-label">
+										Contraseña
+									</label>
+									<input
+										type="password"
+										className="form-control"
+										id="exampleInputPassword1"
+										ref={passwd}
+									/>
+								</div>
+								<div className="justify-content-center d-flex">
+									<button
+										type="button"
+										className="btn btn-light mb-5"
+										onClick={() => {
+											actions.login(username.current.value, passwd.current.value).catch(error => {
+												setAlert("Usuario o contraceña equivocado");
+											});
+										}}>
+										Aceptar
+									</button>
+								</div>
+							</form>
+						</div>
 					</div>
 				</div>
-			</div>
+			) : (
+				<h3>Bienvenido</h3>
+			)}
 		</div>
 	);
 };
